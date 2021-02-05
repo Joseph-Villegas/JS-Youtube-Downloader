cd4e723ce7a27e7774726c3d246e7764eb81b797
Joseph-Villegas <jovillegas@csumb.edu>
2021-02-04 21:45:01 -0800

    added mp4 dwnld api

--- routes/download.js
+++ routes/download.js
@@ -25,4 +25,24 @@ router.get("/mp3", async (req, res) => {
     }
 });
 
+router.get("/mp4", async (req, res) => {
+    if (!req.query.url) return res.json({ success: false, msg: "Missing URL" }); 
+
+    const url = req.query.url;
+    let title = "video";
+
+    try {
+        // Get title of video
+        const info = await ytdl.getBasicInfo(url, { format: "mp4" });
+        title = info.player_response.videoDetails.title.replace(/[^\x00-\x7F]/g, "");
+  
+        // Download video with filename = video title
+        res.header("Content-Disposition", `attachment; filename="${title}.mp4"`);
+        ytdl(url, { format: "mp4" }).pipe(res);
+    } catch (err) {
+      console.error(err);
+      return res.json({ success: false, msg: "Invalid URL" });
+    }
+});
+
 module.exports = router;
