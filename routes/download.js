bdd6ba8a64ae7121442c5a3d97f9bc8c34cbf1d2
Joseph-Villegas <jovillegas@csumb.edu>
2021-02-04 21:40:11 -0800

    mp3 dwnld complete

--- routes/download.js
+++ routes/download.js
@@ -6,22 +6,22 @@ const router = express.Router();
 router.use(cors());
 
 router.get("/mp3", async (req, res) => {
-    if (!req.query.url) return res.json({ success: false, msg: "Invalid/Missing Input (URL)" }); 
+    if (!req.query.url) return res.json({ success: false, msg: "Missing URL" }); 
     
-    try {
-      const url = req.query.url;
+    const url = req.query.url;
+    let title = "audio"; // set a default title
 
-      let title = "audio";
-  
-      const info = await ytdl.getBasicInfo(url, { format: "mp4" });
-      title = info.player_response.videoDetails.title.replace(/[^\x00-\x7F]/g, "");
-  
-  
-      res.header("Content-Disposition", `attachment; filename="${title}.mp3"`);
-      ytdl(url, { format: "mp3", filter: "audioonly" }).pipe(res);
+    try {
+        // Get title of video
+        const info = await ytdl.getBasicInfo(url, { format: "mp4" });
+        title = info.player_response.videoDetails.title.replace(/[^\x00-\x7F]/g, "");
+
+        // Download video with filename = video title
+        res.header("Content-Disposition", `attachment; filename="${title}.mp3"`);
+        ytdl(url, { format: "mp3", filter: "audioonly" }).pipe(res);
     } catch (err) {
       console.error(err);
-      res.json({ success: false, msg: "Invalid URL" });
+      return res.json({ success: false, msg: "Invalid URL" });
     }
 });
 
