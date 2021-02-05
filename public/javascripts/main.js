5a4bc1b81cc1319ae6c0444f57d5f891cdce3ae3
Joseph-Villegas <jovillegas@csumb.edu>
2021-02-04 22:52:54 -0800

    connected search form to backend - need display function

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -12,3 +12,18 @@ downloadForm.addEventListener("submit", async event => {
     const endpoint = fileType == "mp3" ? `/download/mp3?url=${String(url)}` : `/download/mp4?url=${String(url)}`;
     window.location.href = endpoint;
 });
+
+const searchForm = document.getElementById("search");
+searchForm.addEventListener("submit", async event => {
+    // Keep form data from reaching server before being validated
+    event.preventDefault();
+
+    const title = document.getElementById("title").value.trim();
+    // Validate input length
+    if (!title.length) { console.log("Empty Title Field"); return; }
+
+    const endpoint = `/search?title=${title}`;
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    console.log(data);
+});
