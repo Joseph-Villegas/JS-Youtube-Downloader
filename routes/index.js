203a0f36d0fc967383bbb2ab9e78b39961497d99
Joseph-Villegas <jovillegas@csumb.edu>
2021-02-04 22:52:30 -0800

    added search route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
+const express = require('express');
+const fetch = require("node-fetch");
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'YouTube Downloader' });
+});
+
+/* Search for a YouTube video by title using the YouTube API */
+router.get('/search', async (req, res) => {
+  if (!req.query.title) return res.json({ success: false, msg: "Missing search term" });
+
+  const endpoint = `https://www.googleapis.com/youtube/v3/search?key=${process.env.API_KEY}&part=snippet&q=${req.query.title}&maxResults=${10}&type=video`;
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  return res.json(data);
 });
 
 module.exports = router;
